ed0cf92ccacbe6ae4e62faa1e0cc166c8a364659
stehlfi1 <filip.stehlik@skippay.cz>
2024-10-17 09:59:05 +0200

    Added variant to emailtemplate for A/B testing support

--- pymess/utils/migrations.py
+++ pymess/utils/migrations.py
@@ -25,19 +25,20 @@ def get_email_template_body_from_file(email_template_slug, locale):
 
 class SyncEmailTemplates:
 
-    def __init__(self, template_slugs=None, locale=None):
+    def __init__(self, template_slugs=None, locale=None, variant=None):
         self.template_slugs = template_slugs
         self.locale = locale
+        self.variant = variant
 
     def __call__(self, apps, schema_editor):
         email_template_class = apps.get_model(*settings.EMAIL_TEMPLATE_MODEL.split('.'))
         email_template_qs = email_template_class.objects.all()
         if self.template_slugs:
-            email_template_qs = email_template_qs.filter(slug__in=self.template_slugs, locale=self.locale)
+            email_template_qs = email_template_qs.filter(slug__in=self.template_slugs, locale=self.locale, variant=self.variant)
         email_templates_found = list(email_template_qs)
         if self.template_slugs:
             email_templates_not_found = set(self.template_slugs) - {t.slug for t in email_templates_found}
             assert not email_templates_not_found, f"Email templates {email_templates_not_found} were not found."
         for email_template in email_templates_found:
-            email_template.body = get_email_template_body_from_file(email_template.slug, self.locale)
+            email_template.body = get_email_template_body_from_file(email_template.slug, self.locale, self.variant)
         email_template_class.objects.bulk_update(email_templates_found, ['body'])
